be21bb6f6f85d32976fd01115fea07cd95fc11ad
boui <boui@squareliquid.(none)>
2012-03-01 22:00:44 +0300

    Commiting basic maven project with scala support, closed #1

---
+++ src/main/scala/App.scala
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: boui
+ * Date: 2/29/12
+ * Time: 1:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class App {
+  def main(args:Array[Int]){
+    println("Hi!")
+  }
+}
