560ee96bd9cb7f8f6a3b08a402321b0c981b956a
Igor Petruk <igor.petrouk@gmail.com>
2012-03-01 14:47:48 +0200

    Added MongoDB connection. Added Iterable conversion and field selection mockups. All typesafe. #3

--- furrymap-design/src/main/scala/com/petruk/App.scala
+++ furrymap-design/src/main/scala/com/petruk/App.scala
@@ -2,6 +2,8 @@ package com.petruk
 
 import java.lang.reflect.{Method, InvocationHandler}
 import net.sf.cglib.proxy.{Enhancer, MethodProxy, MethodInterceptor}
+import collection.JavaConversions._
+import com.mongodb.{BasicDBObject, Mongo}
 
 trait Entity{}
 
@@ -55,9 +57,34 @@ class MyInterceptor extends MethodInterceptor{
   }
 }
 
+object TransactionIsolation extends Enumeration{
+  type TransactionIsolation = Value
+  val READ_UNCOMMITED = Value
+}
+
+import TransactionIsolation._
+
+class TransactionFlags
+object ForceNew extends TransactionFlags
+object SomeOther extends TransactionFlags
+
+object transaction{
+  def apply
+  (flags:TransactionFlags*)
+  (f: => Unit){
+    f
+  }
+  def apply (f: => Unit):Unit = apply()(f)
+}
+
 trait Selector{
   type K
-  def where(expression: K=>Boolean)(implicit m: Manifest[K]):List[K] = {
+}
+
+trait IterableSelector[B] extends Selector with Iterable[B]{
+  type K=B
+  def where(expression: K=>Boolean)(implicit m: Manifest[K]):this.type = {
+    println(m.erasure)
     if (m.erasure==classOf[Awesome]){
       val enhancer = new Enhancer();
       enhancer.setSuperclass(classOf[Awesome]);
@@ -70,13 +97,28 @@ trait Selector{
         println(k)
       }
     }
-    return List[K]()
+    this
+  }
+
+  def only(queries: (K=>Any)*)(implicit m: Manifest[K]):this.type={
+    println(m.erasure)
+    for (q<-queries){
+      q(null.asInstanceOf[K])
+    }
+    this
+  }
+
+  def iterator:Iterator[K] = {
+    println("Making iterator")
+    List[K]().iterator
   }
 }
 
 class Data(o: Object){
    def select[T <: Entity](implicit m: Manifest[T])={
-      new Selector{type K = T}
+      new IterableSelector[T](){
+        
+      }
    }
 }
 
@@ -107,12 +149,39 @@ object App
 
   implicit def i2c(i:Int):Operation = new Operation(i)
 
+  implicit def sel2it[T](i: Selector{type K=T}):Iterable[T]=null
+
     def main(args: Array[String]):Unit={
        code(true !& false)
         val data = new Data(null)
         import data._
        
-      val k = select[Awesome] where {x=>x.field1 !> x.field2}
+      val k = select[Awesome].only(_.field1).where(x=>x.field1 !> 0).map(2*_.field1)
 
+      transaction(SomeOther, ForceNew){
+        transaction(){
+
+        }
+      }
+
+      val mongo = new Mongo();
+      val db = mongo.getDB("furrymap-design");
+      for (collection<-db.getCollectionNames)
+        println(collection)
+      
+      val collection = db.getCollection("collection1");
+      collection.drop();
+
+      for (i<-0 to 1000000){
+        val obj = new BasicDBObject();
+        obj.put("test", "some");
+        collection.insert(obj)
+      }
+
+
+      /*for(a<-asScalaIterator(collection.find())){
+        println(a)
+      } */
+      
     }
 }
